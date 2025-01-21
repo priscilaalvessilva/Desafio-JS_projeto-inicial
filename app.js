3dcdd23dbdc550c6e2df303f1b4206e90b5b6dc7
PRISCILA ALVES <75861909+priscilaalvessilva@users.noreply.github.com>
2025-01-21 07:44:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+alert ("Bem-vindos ao nosso site!")
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha os campos obrigatórios!";
+
+alert (mensagemDeErro);
+
+let mome = prompt ("Qual é o seu nome?");
+let idade = prompt ("Qual é a sua idade?");
+
+if (idade >= 18) {
+    alert ("Pode tirar a habilitação!");
+}
+
+else {
+    alert ("Aguarde mais um pouco para tirar a habilitação!");
+}
+
+
+
